225034b43f28cfaca351cc67505cc0f16f272114
Mila Votradovec <mila@snyk.io>
2020-01-15 15:42:41 +0200

    feat: clojure-deps parser

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,41 @@
 import 'source-map-support/register';
+import {DepGraph, DepGraphBuilder, PkgInfo} from "@snyk/dep-graph";
+import * as os from "os";
 
-export default function(): boolean {
-  return true;
+export function parseClojureDeps(name: string, version: string, depsEdnContent: string): DepGraph {
+  const rootPackage: PkgInfo = {name, version};
+  const depGraph = new DepGraphBuilder({name: 'clojure-deps'}, rootPackage);
+  const parentsStack: string[] = [];
+  for (const line of depsEdnContent.trimRight().split(os.EOL)) {
+    // count level of nesting
+    const currentLevel = countSpaces(line) / 2;
+    const trimmedLine = line.trimLeft();
+
+    // go back in stack to the last parent of same level
+    parentsStack.splice(currentLevel);
+
+    // parse package and add it to the graph
+    const [pkgName, pkgVersion] = trimmedLine.split(' ');
+    const pkg: PkgInfo = {name: pkgName, version: pkgVersion};
+    const currentNodeId = `${pkgName}@${pkgVersion}`;
+    depGraph.addPkgNode(pkg, currentNodeId);
+    if (currentLevel === 0) {
+      depGraph.connectDep(depGraph.rootNodeId, currentNodeId);
+    } else {
+      depGraph.connectDep(parentsStack.slice(-1)[0], currentNodeId);
+    }
+    // save current node as potential parent
+    parentsStack.push(currentNodeId);
+  }
+
+  return depGraph.build();
+}
+
+function countSpaces(input: string): number {
+  let i = 0;
+  while (input[i] === ' ') {
+    i++;
+  }
+
+  return i;
 }
--- test/lib/index.test.ts
+++ test/lib/index.test.ts
@@ -1,5 +1,21 @@
-import fun from "../../lib";
+import { parseClojureDeps } from "../../lib";
+import * as path from "path";
+import * as fs from "fs";
+import { createFromJSON, DepGraphData } from "@snyk/dep-graph";
 
-test("Is everything ready for the development?", async () => {
-  expect(fun()).toBeTruthy();
+test("Valid TreeParsing", async () => {
+  const validTreeContent = fs.readFileSync(
+    path.resolve(__dirname, "../fixtures/valid-tree"),
+    "utf-8"
+  );
+  const fixtureDepGraphContent = JSON.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, "../fixtures/valid-tree-dep-graph.json"),
+      "utf-8"
+    )
+  ) as DepGraphData;
+  const resDepGrah = parseClojureDeps("testing", "0.0.1", validTreeContent);
+  const fixtureDepGraph = createFromJSON(fixtureDepGraphContent);
+
+  expect(resDepGrah.equals(fixtureDepGraph)).toBeTruthy();
 });
