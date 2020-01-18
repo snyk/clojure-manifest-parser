3aa20f733796a0bfa1eca3e2139a06f9b49692a3
Aviad Reich <aviad@snyk.io>
2020-01-18 15:48:22 +0200

    feat: parser for `lein deps :tree`

--- lib/index.ts
+++ lib/index.ts
@@ -1,22 +1,74 @@
-import 'source-map-support/register';
-import {DepGraph, DepGraphBuilder, PkgInfo} from "@snyk/dep-graph";
+import "source-map-support/register";
+import { DepGraph, DepGraphBuilder, PkgInfo } from "@snyk/dep-graph";
 import * as os from "os";
 
-export function parseClojureDeps(name: string, version: string, depsEdnContent: string): DepGraph {
-  const rootPackage: PkgInfo = {name, version};
-  const depGraph = new DepGraphBuilder({name: 'clojure-deps'}, rootPackage);
+export function parseLein(
+  name: string,
+  version: string,
+  leinDepsTreeContent: string
+): DepGraph {
+  function parseLine(line: string) {
+    return line
+      .replace("[", "")
+      .trimLeft()
+      .split(" ")
+      .slice(0, 2);
+  }
+  function calcDepth(line: string) {
+    return (countSpaces(line) - 1) / 2;
+  }
+
+  return parseTextTree(
+    name,
+    version,
+    leinDepsTreeContent,
+    calcDepth,
+    parseLine
+  );
+}
+
+export function parseClojureDeps(
+  name: string,
+  version: string,
+  depsEdnDepsTreeContent: string
+): DepGraph {
+  function parseLine(line: string) {
+    return line.split(" ").slice(0, 2);
+  }
+  function calcDepth(line: string) {
+    return countSpaces(line) / 2;
+  }
+
+  return parseTextTree(
+    name,
+    version,
+    depsEdnDepsTreeContent,
+    calcDepth,
+    parseLine
+  );
+}
+
+function parseTextTree(
+  name: string,
+  version: string,
+  treeContent: string,
+  calcDepth: (input: string) => number,
+  parseLine: (line: string) => string[]
+): DepGraph {
+  const rootPackage: PkgInfo = { name, version };
+  const depGraph = new DepGraphBuilder({ name: "clojure-deps" }, rootPackage);
   const parentsStack: string[] = [];
-  for (const line of depsEdnContent.trimRight().split(os.EOL)) {
+  for (const line of treeContent.trimRight().split(os.EOL)) {
     // count level of nesting
-    const currentLevel = countSpaces(line) / 2;
-    const trimmedLine = line.trimLeft();
+    const currentLevel = calcDepth(line);
+    const trimmedLine = line.trimLeft(); // ASSUMPTION: depth is only whitespaces/tabs
 
     // go back in stack to the last parent of same level
     parentsStack.splice(currentLevel);
 
     // parse package and add it to the graph
-    const [pkgName, pkgVersion] = trimmedLine.split(' ');
-    const pkg: PkgInfo = {name: pkgName, version: pkgVersion};
+    const [pkgName, pkgVersion] = parseLine(trimmedLine);
+    const pkg: PkgInfo = { name: pkgName, version: pkgVersion };
     const currentNodeId = `${pkgName}@${pkgVersion}`;
     depGraph.addPkgNode(pkg, currentNodeId);
     if (currentLevel === 0) {
@@ -33,7 +85,7 @@ export function parseClojureDeps(name: string, version: string, depsEdnContent: 
 
 function countSpaces(input: string): number {
   let i = 0;
-  while (input[i] === ' ') {
+  while (input[i] === " ") {
     i++;
   }
 
